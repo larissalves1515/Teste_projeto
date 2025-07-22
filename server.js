a593ff0dfe054b199e4780c8a4444dcd902876d9
larissalves1515 <larissavioleta15@gmail.com>
2025-07-22 19:10:23 -0300

    b

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-import express from 'express'
-
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORTA = 3000;
-let itens = [1, 2, 3, 4];
+let itens = [];
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/itens', (req, res) => {
-  const itens = [
-    {id: 1, nome: 'Joao'},
-    {id: 2, nome: 'Ana' }
-  ]
   res.json(itens);
 });
 
-app.post('/itens', (req, res)  => {
-  const novoItem = req.body;
-console.log('Novo item recebido:', novoItem);
-res.status(201).json({mensagem: 'Item criado com sucesso!', item: novoItem
-
-})
-})
-
-
+app.post('/itens', (req, res) => {
+  const {id, nome} = req.body;
+  if(!id || !nome){
+    return res.status(400).json({mensagem: 'ID e nome obrigatorio'});
+  }
+  const novoItem = {id, nome};
+  itens.push(novoItem);
+  console.log('Novo item recebido:', novoItem);
+  res.status(201).json({
+    mensagem: 'Item criado com sucesso!',
+    item: novoItem
+  });
+});
 
 app.listen(PORTA, () => {
-    console.log(`Servidor rodando em http://localhost:${PORTA}`);
-})
-
-
-
-
+  console.log(`Servidor rodando em http://localhost:${PORTA}`);
+});
